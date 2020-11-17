6c56aac74fc9761618fa9dcedc855bd2ab70b1e5
mirdukkk <vdvmirduk@gmail.com>
2020-11-18 00:11:40 +0300

    channel create system

--- src/events/voice/VoiceStateUpdateEvent.ts
+++ src/events/voice/VoiceStateUpdateEvent.ts
@@ -8,7 +8,7 @@ export = class VoiceStateUpdateEvent extends Event {
     })
   }
 
-  execute(oldState: Discord.VoiceState, newState: Discord.VoiceState) {
+  async execute(oldState: Discord.VoiceState, newState: Discord.VoiceState) {
     if (oldState.guild.id !== this.client.guildID) return
 
     if (this.client.cache.voices.get(newState.member.user.id)) {
@@ -21,16 +21,47 @@ export = class VoiceStateUpdateEvent extends Event {
           })
         else {
           if (newState.guild.channels.cache.get(oldState.channelID)?.members.size === 0) {
+            this.client.cache.voices.delete(newState.member.user.id)
             return newState.guild.channels.cache.get(oldState.channelID).delete(
               'Автоматическое удаление пустого приватного голосового канала.'
             )
           }
         }
+      } else if (newState.channelID === this.client.config.voiceChannelID) {
+        return newState.member.edit({
+          channel: voice.channelID
+        })
       }
 
     } else {
       if (newState.channelID === this.client.config.voiceChannelID) {
+        const voice = await newState.guild.channels.create(
+          '✨ Канал от ' + newState.member.displayName,
+          {
+            parent: this.client.config.voiceChannelParentID,
+            type: 'voice',
+            position: 0
+          }
+        )
 
+        this.client.cache.voices.set(newState.member.user.id, {
+          channelID: voice.id,
+          owner: newState.member.user.id
+        })
+
+        return newState.member.edit({
+          channel: voice
+        })
+      }
+    }
+
+    if (newState.guild.channels.cache.get(oldState.channelID)?.members.size === 0) {
+      const voice = this.client.cache.voices.find(v => v.channelID === oldState.channelID)
+      if (voice) {
+        this.client.cache.voices.delete(voice.owner)
+        return newState.guild.channels.cache.get(voice.channelID).delete(
+          'Автоматическое удаление пустого приватного голосового канала.'
+        )
       }
     }
   }
