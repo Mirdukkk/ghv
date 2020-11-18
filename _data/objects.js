0ef68666936f78a4e30272a56b62815f12be7d3f
mirdukkk <vdvmirduk@gmail.com>
2020-11-18 20:00:31 +0300

    delete old files

--- _data/objects.js
+++
@@ -1,5 +0,0 @@
-const { Collection } = require('discord.js')
-module.exports = {
-    voiceCd: new Set(),
-    createdVoices: new Collection()
-} 
--- _events/ready.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client) => {
-    console.log(`${client.user.username} Turn on!`)
-    client.user.setActivity(`!помощь`, { type: 'STREAMING', url: 'https://www.twitch.tv/le1zz' })
-}
-
